b7c7a92594016ebca2a8fc35afe6d322a3bc8877
Adam Frej <awfrej@googlemail.com>
2018-09-10 17:25:45 -0400

    able to send and add new messages to current room

---
+++ src/components/SendMessageForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class SendMessageForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+    this.handleFormChange = this.handleFormChange.bind(this)
+    this.handleSubmit= this.handleSubmit.bind(this)
+  }
+
+  handleFormChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+    if (this.props.onChange) {
+      this.props.handleFormChange()
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.onSubmit(this.state.text)
+    this.setState({text:''})
+  }
+    render() {
+      return (
+        <form className="send-message-form" onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleFormChange}
+            placeholder="Type your message here"
+            type="text"
+            value={this.state.text} />
+        </form>
+      )
+    }
+  }
+
+  export default SendMessageForm
