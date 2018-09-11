be16d32eb19fb4d5420e136e876d9a472d530b0d
Adam Frej <awfrej@googlemail.com>
2018-09-11 15:02:26 -0400

    layout changes for readability

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,8 +4,8 @@ import Message from './Message'
 class MessageList extends React.Component {
 
   componentWillUpdate() {
-
     // check if client is reading earlier messages
+
     const node = ReactDOM.findDOMNode(this)
     this.scrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
   }
@@ -13,6 +13,7 @@ class MessageList extends React.Component {
 
   componentDidUpdate() {
     // scroll to bottom ^^ only if we arent reading top
+
     if (this.scrollToBottom) {
       const node = ReactDOM.findDOMNode(this)
       node.scrollTop = node.scrollHeight
