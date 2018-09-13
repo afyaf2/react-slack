62198bb92b30922342c18b6ad3e8448555e226d0
Adam Frej <awfrej@googlemail.com>
2018-09-13 14:17:16 -0400

    changes to package.json scripts

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.listen(process.env.PORT || 3001, err => {
+app.listen(process.env.PORT || 5000, err => {
   if (err) {
     console.error(err)
   } else {
